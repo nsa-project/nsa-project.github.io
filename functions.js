0646623fa76b05fc7d11d5dbbc95f987652e8447
nsa-project <120071287+nsa-project@users.noreply.github.com>
2022-12-08 18:00:16 -0800

    Make plan and close gallery button

--- functions.js
+++ functions.js
@@ -9,8 +9,12 @@ function setValueById(id, value)  { //set value of element
     document.getElementById(id).value = value
 }
 function planBT() { //plan button onclicked
+    setinfo('Windmills OR solar panels will be installed on the school campus aswell as a battery backup and LED lights in both NSA buildings. The windmills will be small and fenced off so that scholars will not be hurt or tamper with the windmills. <br> The windmills or solar panels will have to be maintained by janitorial services or other trained staff members. The battery backup will also be fenced in to prevent tampering, and will be connected to solar panels or windmills, city power supply, and the school main supply. <br> In case of a battery backup failure, there will be a manual bypass switch to connect city power directly to the school power line.');
+    var img = document.getElementById('image');
+    img.src = 'https://raw.githubusercontent.com/nsa-project/nsa-project.github.io/main/img/plan.png'
+}
+function closeGallery() {
     setinfo('');
     var img = document.getElementById('image');
-    img.src = 'https://png.pngtree.com/png-clipart/20210206/ourmid/pngtree-big-red-flower-with-black-border-clipart-png-image_2890436.jpg'
-    alert('click');
+    img.src = ''
 }
