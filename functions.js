7f977075471e7699ad2cdac13104f53493583444
nsa-project <120071287+nsa-project@users.noreply.github.com>
2022-12-08 17:01:19 -0800

    plan test

--- functions.js
+++ functions.js
@@ -1,10 +1,15 @@
-function setinfo(cont) {
+function setinfo(cont) { //set txt of gallery div
     var out = document.getElementById("galleryContent");
     out.innerHTML = cont
 }
-function getValueById(id) {
+function getValueById(id) { //get value of element
     return document.getElementById(id).value
 }
-function setValueById(id, value)  {
+function setValueById(id, value)  { //set value of element
     document.getElementById(id).value = value
+}
+function planBT() { //plan button onclicked
+    setinfo('');
+    var img = document.getElementById('image')
+    img.src = 'https://png.pngtree.com/png-clipart/20210206/ourmid/pngtree-big-red-flower-with-black-border-clipart-png-image_2890436.jpg'
 }
