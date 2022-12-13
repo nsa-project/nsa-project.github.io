ccfe8adb267192211a54c62f98e59d807a35e695
nsa-project <120071287+nsa-project@users.noreply.github.com>
2022-12-12 17:08:11 -0800

    asdfasdg

--- functions.js
+++ functions.js
@@ -17,7 +17,8 @@ function planBT() { //plan button onclicked
     setinfo('Windmills OR solar panels will be installed on the school campus as well as a battery backup and LED lights in both NSA buildings. The windmills will be small and fenced off so that scholars will not be hurt or tamper with the windmills. <br> The windmills or solar panels will have to be maintained by janitorial services or other trained staff members. The battery backup will also be fenced in to prevent tampering, and will be connected to solar panels or windmills, city power supply, and the school main supply. <br> In case of a battery backup failure, there will be a manual bypass switch to connect city power directly to the school power line.');
     var img = document.getElementById('image');
     img.src = 'https://raw.githubusercontent.com/nsa-project/nsa-project.github.io/main/img/plan.png';
-    setValueById('prev', 'Plan')
+    var prev = document.getElementById('prev');
+    prev.innerHTML = 'Plan'
 }
 function closeGallery() {
     setinfo('');
@@ -27,5 +28,5 @@ function closeGallery() {
     var out = document.getElementById("galleryContent");
     out.style.margin = '0%';
     var prev = document.getElementById('prev');
-    prev.value = ''
+    prev.innerHTML = ''
 }
