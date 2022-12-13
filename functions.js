ebaf4c409d5fb180b87193d1bbd9417f6e1864f5
nsa-project <120071287+nsa-project@users.noreply.github.com>
2022-12-12 16:40:26 -0800

    previeu

--- functions.js
+++ functions.js
@@ -12,11 +12,15 @@ function getValueById(id) { //get value of element
 }
 function setValueById(id, value)  { //set value of element
     document.getElementById(id).value = value
+    var prev = document.getElementById('prev');
+    prev.value = 'Info'
 }
 function planBT() { //plan button onclicked
     setinfo('Windmills OR solar panels will be installed on the school campus as well as a battery backup and LED lights in both NSA buildings. The windmills will be small and fenced off so that scholars will not be hurt or tamper with the windmills. <br> The windmills or solar panels will have to be maintained by janitorial services or other trained staff members. The battery backup will also be fenced in to prevent tampering, and will be connected to solar panels or windmills, city power supply, and the school main supply. <br> In case of a battery backup failure, there will be a manual bypass switch to connect city power directly to the school power line.');
     var img = document.getElementById('image');
-    img.src = 'https://raw.githubusercontent.com/nsa-project/nsa-project.github.io/main/img/plan.png'
+    img.src = 'https://raw.githubusercontent.com/nsa-project/nsa-project.github.io/main/img/plan.png';
+    var prev = document.getElementById('prev');
+    prev.value = 'Plan'
 }
 function closeGallery() {
     setinfo('');
@@ -25,8 +29,6 @@ function closeGallery() {
     document.getElementById('closeBT').style.display = 'none';
     var out = document.getElementById("galleryContent");
     out.style.margin = '0%';
-}
-function indicateInfo() {
-    $('planButton').css("border-style", "none");
-    $('infoButton').css("border-style", "solid");
+    var prev = document.getElementById('prev');
+    prev.value = ''
 }
